cfe331ae5d1e2b415a74d584fce4421590149d47
KimSungKyoung <sungkyoung1991@gmail.com>
2018-01-18 15:39:21 +0900

    efef

--- src/main/java/com/sample/choc2/common/aspect/LogAspectJ.java
+++ src/main/java/com/sample/choc2/common/aspect/LogAspectJ.java
@@ -15,11 +15,8 @@ package com.sample.choc2.common.aspect;
 
 
 //git test ------parksujin
-<<<<<<< HEAD
 //git tt -sk
-=======
 //git testest ------sujin
 
 //git testing 2
 //testestestestestest sujin
->>>>>>> branch 'master' of git@gitlab.com:nainfox/server/choc2_server.git
