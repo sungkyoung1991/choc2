8596d7b97a18c8ff2b4a03eed0317aa03aa4f7a3
KimSungKyoung <sungkyoung1991@gmail.com>
2018-02-08 09:24:58 +0900

    sk test commit

--- src/test/java/com/sample/choc2/UserServiceTest.java
+++ src/test/java/com/sample/choc2/UserServiceTest.java
@@ -14,7 +14,7 @@ import com.sample.choc2.service.user.UserService;
 @RunWith(SpringJUnit4ClassRunner.class)
 
 @ContextConfiguration	(locations = {	"classpath:config/context-common.xml",
-											"classpath:config/context-datasource.xml",
+											"classpath:config/context-mybatis.xml",
 												"classpath:config/context-mybatis.xml"	})
 public class UserServiceTest {
 
