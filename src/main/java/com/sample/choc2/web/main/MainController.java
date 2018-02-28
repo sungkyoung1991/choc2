a8ef88317b9da6efd130ee1bb5a4f6c368b67486
show3r <show3r.sujin@gamil.com>
2018-02-28 17:01:29 +0900

    sujin update

--- src/main/java/com/sample/choc2/web/main/MainController.java
+++ src/main/java/com/sample/choc2/web/main/MainController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.sample.choc2.common.Page;
 import com.sample.choc2.common.Search;
 import com.sample.choc2.service.main.MainService;
-import com.sample.choc2.service.product.ProductService;
 
 @Controller
 @RequestMapping("/main/*")
@@ -22,10 +21,7 @@ public class MainController {
 	@Qualifier("mainServiceImpl")
 	private MainService mainService;
 	
-	@Autowired
-	@Qualifier("productServiceImpl")
-	private ProductService productService;
-	
+
 	@Value("#{commonProperties['pageUnit']}")
 	int pageUnit;
 
@@ -37,20 +33,5 @@ public class MainController {
 		super();
 	}
 	
-	@RequestMapping(value="mainModule", method=RequestMethod.GET)
-	public String getProductList(@ModelAttribute("search") Search search, @ModelAttribute("page") Page page, Model model) throws Exception{
-		
-		if (search.getCurrentPage() == 0) {
-			search.setCurrentPage(1);
-		}
-		search.setPageSize(pageSize);
-		
-		model.addAttribute("productList",productService.getProductList(search).get("list"));
-		
-		Page resultPage = new Page(search.getCurrentPage(), ((Integer) productService.getProductList(search).get("totalCount")).intValue(), pageUnit,
-				pageSize);
-		model.addAttribute("resultPage",resultPage);
-		
-		return "main/main";
-	}
+
 }
