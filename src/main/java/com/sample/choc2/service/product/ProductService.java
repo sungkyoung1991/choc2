6116a41ef018994214214ad52e15b8d9f3bd0634
KimSungKyoung <sungkyoung1991@gmail.com>
2018-03-07 15:13:49 +0900

    sk_asepect_query

--- src/main/java/com/sample/choc2/service/product/ProductService.java
+++ src/main/java/com/sample/choc2/service/product/ProductService.java
@@ -4,14 +4,15 @@ import java.util.Map;
 
 import com.sample.choc2.common.Search;
 import com.sample.choc2.service.domain.Product;
+import com.sample.choc2.service.domain.UserVO;
 
 public interface ProductService {
 	
 	public void addProduct(Product product) throws Exception;
 	
-	public Product getProduct(int prodNo) throws Exception;
+	public Product getProduct(UserVO user, int prodNo) throws Exception;
 	
-	public Map<String,Object> getProductList(Search search) throws Exception;
+	public Map<String,Object> getProductList(UserVO user, Search search) throws Exception;
 	
 	public void updateProduct(Product product) throws Exception;
 	
